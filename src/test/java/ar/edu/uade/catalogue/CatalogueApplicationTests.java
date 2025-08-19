5826322f202b0d76059e41cdd5834afcf9dc7855
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2025-08-18 21:25:06 -0300

    Commit inicial

---
+++ src/test/java/ar/edu/uade/catalogue/CatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
