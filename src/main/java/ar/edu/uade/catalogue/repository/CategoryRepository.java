a9f27b53ede43500a2b806d1c52d8e030d9cc05c
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2025-08-21 10:41:54 -0300

    Agregando metodo findByName para las categorias en el repository

--- src/main/java/ar/edu/uade/catalogue/repository/CategoryRepository.java
+++ src/main/java/ar/edu/uade/catalogue/repository/CategoryRepository.java
@@ -1,11 +1,14 @@
 package ar.edu.uade.catalogue.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ar.edu.uade.catalogue.model.Category;
 
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Integer>{
-
+    Optional<Category> findByName(String name);
 }
