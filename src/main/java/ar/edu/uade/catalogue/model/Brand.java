1165375824e70ce680174d49fc13b559c7756b15
Sebas <bernaseba1@gmail.com>
2025-09-06 12:52:16 -0300

    Agregando anotaciones y atributos

--- src/main/java/ar/edu/uade/catalogue/model/Brand.java
+++ src/main/java/ar/edu/uade/catalogue/model/Brand.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,6 +20,7 @@ import lombok.Setter;
 @Getter
 @Setter
 
+@Entity
 @Table(name = "brand")
 public class Brand {
 
--- src/main/java/ar/edu/uade/catalogue/model/Category.java
+++ src/main/java/ar/edu/uade/catalogue/model/Category.java
@@ -1,9 +1,14 @@
 package ar.edu.uade.catalogue.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,6 +20,7 @@ import lombok.Setter;
 @Getter
 @Setter
 
+@Entity
 @Table(name = "category")
 public class Category {
 
@@ -26,8 +32,8 @@ public class Category {
     @Column(name = "name")
     private String name;
     
-    /*@OneToMany(mappedBy="categories", cascade=CascadeType.ALL)
-    List<Product>products;*/
+    @OneToMany(mappedBy="categories", cascade=CascadeType.ALL)
+    List<Product>products;
 
     @Column(name="active", nullable=false)
     private boolean active;
