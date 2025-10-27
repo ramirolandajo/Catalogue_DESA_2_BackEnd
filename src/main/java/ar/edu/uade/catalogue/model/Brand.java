f63995192dd41800ada1a9b95fa4cf5802eef8e7
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2025-10-27 11:30:51 -0300

    Modificando mapeo de productos

--- src/main/java/ar/edu/uade/catalogue/model/Brand.java
+++ src/main/java/ar/edu/uade/catalogue/model/Brand.java
@@ -3,11 +3,15 @@ package ar.edu.uade.catalogue.model;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -36,8 +40,10 @@ public class Brand {
     private String name;
 
     //@Column(name = "products")
-    @OneToMany(mappedBy = "brand")
-    private List<Product>products;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "brand_products", joinColumns = @JoinColumn(name = "brand_id"))
+    @Column(name = "product_id")
+    private List<Integer>products;
 
     @Column(name="active", nullable=false)
     private boolean active;
--- src/main/java/ar/edu/uade/catalogue/model/Category.java
+++ src/main/java/ar/edu/uade/catalogue/model/Category.java
@@ -3,11 +3,15 @@ package ar.edu.uade.catalogue.model;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -37,8 +41,10 @@ public class Category {
     private String name;
     
     //@Column(name = "products")
-    @ManyToMany(mappedBy = "categories")
-    List<Product>products;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "category_products", joinColumns = @JoinColumn(name = "category_id"))
+    @Column(name = "product_id")
+    List<Integer>products;
 
     @Column(name="active", nullable=false)
     private boolean active;
